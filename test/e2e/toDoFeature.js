b92124b04489314ed5b03e65471b4e63b4b1745c
Ashleigh Maund <ashleighmaund@gmail.com>
2015-06-14 22:50:35 +0100

    passing feature tests - can count added tasks on web page

--- test/e2e/toDoFeature.js
+++ test/e2e/toDoFeature.js
@@ -15,7 +15,26 @@ describe('ToDoList', function() {
     });
 
     it('has a submit button', function() {
-      expect(browser.isElementPresent(by.id('button'))).toBe(true);
+      expect(browser.isElementPresent(by.id('addTaskId'))).toBe(true);
     });
+
   });
+
+});
+
+describe('When tasks are added', function(){
+
+  beforeEach(function(){
+    element(by.id('newTaskId')).sendKeys('Walk Larry the lizard');
+    element(by.id('addTaskId')).click();
+  });
+
+  it('has a task count of 1 after first task added', function(){
+    expect(element(by.id('taskCount')).getText()).toEqual('You have 1 toDos!');
+  });
+
+  it('has a task count of 2 when second task added', function(){
+    expect(element(by.id('taskCount')).getText()).toEqual('You have 2 toDos!');
+  });
+
 });
