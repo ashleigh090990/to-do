67ea7138c957ad1637163e400e378ef0cbd0b251
ashleigh090990 <ashleighmaund@gmail.com>
2015-09-16 12:37:33 +0100

    amended Procfile and added server.js and start script to package.json

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+var express = require('express');
+ 
+var server = express();
+server.use(express.static(__dirname + '/'));
+ 
+var port = 10001;
+server.listen(port, function() {
+    console.log('server listening on port ' + port);
+});
