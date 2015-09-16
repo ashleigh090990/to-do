f56bdadb829138b32e877d988bbb620dafbd1259
ashleigh090990 <ashleighmaund@gmail.com>
2015-09-16 13:12:04 +0100

    amended server.js - changed port number to work on heroku

--- server.js
+++ server.js
@@ -5,7 +5,5 @@ var express = require('express');
 var server = express();
 server.use(express.static(__dirname + '/'));
  
-var port = 10001;
-server.listen(port, function() {
-    console.log('server listening on port ' + port);
-});
+var port = 5000;
+server.listen(process.env.PORT || port);
